1035c6c6538f39c94f32e68bc97ebd54fc9acf69
Vafa <v.dehghansaei@gmail.com>
2017-12-20 01:49:59 -0800

    - Added a baseline for the nav drawer, all it needs now is the underlying activities. - Added a new Class which takes care of the drawer behaviour, now you dont need the same long block of code on all the activities which use the drawer.

--- TheBlackList/app/src/main/java/suplex/theblacklist/LandingActivity.java
+++ TheBlackList/app/src/main/java/suplex/theblacklist/LandingActivity.java
@@ -1,19 +1,34 @@
 package suplex.theblacklist;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import suplex.theblacklist.login.LoginActivity;
 import suplex.theblacklist.login.RegisterActivity;
+import suplex.theblacklist.navDrawer.NavigationDrawer;
 
 public class LandingActivity extends AppCompatActivity {
+
+    Boolean admin = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing);
-        startActivity(new Intent(this, RegisterActivity.class));
+        if (admin) {
+            setContentView(R.layout.activity_landing_admin);
+        } else {
+            setContentView(R.layout.activity_landing);
+        }
+//        startActivity(new Intent(this, LoginActivity.class));
+        NavigationDrawer mNavDrawer = new NavigationDrawer(this);
 
         //splash screen before login
     }
